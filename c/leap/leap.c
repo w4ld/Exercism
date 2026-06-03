0eeef0095ef537395ae2e683313b5c31ee2ccf15
derek <d_evans@tutamail.com>
2026-06-03 12:41:45 -0700

    pull and implement leap_year

---
+++ c/leap/leap.c
@@ -0,0 +1,11 @@
+#include "leap.h"
+
+bool leap_year(int year){
+    if (year % 4 == 0 && year % 100 != 0) {
+        return true;
+    } else if (year % 400 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
